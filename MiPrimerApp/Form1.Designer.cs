7b625bc2694076de1ba7347ebf9fc2976e41ce86
LordJuani <52964596+LordJuani@users.noreply.github.com>
2019-08-23 12:39:52 -0300

    Add files via upload

---
+++ MiPrimerApp/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace MiPrimerApp
+{
+    partial class Frm1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.directoryEntry1 = new System.DirectoryServices.DirectoryEntry();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Txt_Prueba1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(407, 119);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Txt_Prueba1
+            // 
+            this.Txt_Prueba1.Location = new System.Drawing.Point(271, 119);
+            this.Txt_Prueba1.Name = "Txt_Prueba1";
+            this.Txt_Prueba1.Size = new System.Drawing.Size(100, 20);
+            this.Txt_Prueba1.TabIndex = 1;
+            // 
+            // Frm1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(689, 354);
+            this.Controls.Add(this.Txt_Prueba1);
+            this.Controls.Add(this.button1);
+            this.Name = "Frm1";
+            this.Text = "Forma 1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Frm1_FormClosed);
+            this.Load += new System.EventHandler(this.Frm1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.DirectoryServices.DirectoryEntry directoryEntry1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox Txt_Prueba1;
+    }
+}
+
---
+++ MiPrimerApp/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiPrimerApp
+{
+    public partial class Frm1 : Form
+    {
+        public Frm1()
+        {
+            InitializeComponent();
+        }
+
+        private void Frm1_Load(object sender, EventArgs e)
+        {
+           // MessageBox.Show("Bienvenidos a C#");
+        }
+
+        private void Frm1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+           // MessageBox.Show("Chau Chau . . .");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           /* MouseEventArgs click = (MouseEventArgs)e;
+            if (click.Button == MouseButtons.Left)
+                MessageBox.Show("Se presionó el botón Izquierdo", "Jojo!");
+            if (click.Button == MouseButtons.Right)
+                MessageBox.Show("Se presionó el botón Derecho", "Jeje");
+            if (click.Button == MouseButtons.Middle)
+                MessageBox.Show("Se presionó el botón del medio", "Pervertido!");
+                */
+                if(Txt_Prueba1.Text== "")
+            {
+                Txt_Prueba1.BackColor = Color.Aquamarine;
+            }
+            else
+            {
+                Txt_Prueba1.BackColor = System.Drawing.SystemColors.Control;
+            }
+
+        }
+    }
+    }
+
